8e5249622d978cea0df0704c10f83a6b129f1222
Emodisoon <61479166+Emodisoon@users.noreply.github.com>
2020-02-26 01:54:08 +0300

    Added user

---
+++ src/main/java/com/MitolGames/CubeMaster/repo/RoleRepo.java
@@ -0,0 +1,7 @@
+package com.MitolGames.CubeMaster.repo;
+
+import com.MitolGames.CubeMaster.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/MitolGames/CubeMaster/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.MitolGames.CubeMaster.repo;
+
+import com.MitolGames.CubeMaster.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUserName(String username);
+}
